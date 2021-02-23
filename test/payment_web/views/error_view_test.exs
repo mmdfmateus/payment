807c6296ccb43791fab8d3f20f232e4d1ed077ae
Mateus Mourão <mateusm@take.net>
2021-02-22 22:22:50 -0300

    first commit

---
+++ test/payment_web/views/error_view_test.exs
@@ -0,0 +1,15 @@
+defmodule PaymentWeb.ErrorViewTest do
+  use PaymentWeb.ConnCase, async: true
+
+  # Bring render/3 and render_to_string/3 for testing custom views
+  import Phoenix.View
+
+  test "renders 404.json" do
+    assert render(PaymentWeb.ErrorView, "404.json", []) == %{errors: %{detail: "Not Found"}}
+  end
+
+  test "renders 500.json" do
+    assert render(PaymentWeb.ErrorView, "500.json", []) ==
+             %{errors: %{detail: "Internal Server Error"}}
+  end
+end
